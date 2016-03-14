d31e32f61e0908bc0d8b113d048ce7865b5dff4d
Jonathan Gertig <jcgertig@gmail.com>
2016-03-14 17:14:47 -0400

    working on routing

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/login', to: 'ConveyUser/sessions#new'
+  get '/auth/:provider/callback', to: 'ConveyUser/sessions#create'
+
+  post '/webhooks/convey', to: 'ConveyUser/webhooks#catch'
+end
--- lib/config/routes.rb
+++
@@ -1,6 +0,0 @@
-ConveyUser::Engine.routes.draw do
-  get '/login', to: 'ConveyUser/sessions#new'
-  get '/auth/:provider/callback', to: 'ConveyUser/sessions#create'
-
-  post '/webhooks/convey', to: 'ConveyUser/webhooks#catch'
-end
