ba7769e804228d739136db8b6b0e95596af291b8
Jonathan Gertig <jcgertig@gmail.com>
2016-03-15 14:22:10 -0400

    update params passing

--- app/controllers/convey_user/webhooks_controller.rb
+++ app/controllers/convey_user/webhooks_controller.rb
@@ -5,14 +5,14 @@ module ConveyUser
     def catch
       action = request.headers["X-Convey-Action"] if request.headers["X-Convey-Action"]
       resource = request.headers["X-Convey-Resource"] if request.headers["X-Convey-Resource"]
-      puts params.to_json.to_h
-      send("#{action}_#{resource}", params.to_json.to_h)
+      puts JSON.parse(params.to_json)
+      send("#{action}_#{resource}", JSON.parse(params.to_json))
     end
 
     private
 
       def update_users(params)
-        threads = params[:users].each do |user|
+        threads = params['users'].each do |user|
           Thread.new do
             ActiveRecord::Base.connection_pool.with_connection do
               uid = user[:_id]
@@ -32,24 +32,24 @@ module ConveyUser
 
       def update_user(params)
         puts params
-        user = params[:user]
-        uid  = user[:_id]
-        user.delete(:_id)
+        user = params['user']
+        uid  = user['_id']
+        user.delete('_id')
         puts user
         ::User.where(uid: uid).first_or_create(user).update(user)
       end
 
       def create_user(params)
-        user = params[:user]
-        uid  = user[:_id]
-        user.delete(:_id)
+        user = params['user']
+        uid  = user['_id']
+        user.delete('_id')
         ::User.where(uid: uid).first_or_create(user)
       end
 
       def destroy_user(params)
-        user = params[:user]
-        uid  = user[:_id]
-        user.delete(:_id)
+        user = params['user']
+        uid  = user['_id']
+        user.delete('_id')
         ::User.where(uid: uid).first.update(active: false)
       end
   end
