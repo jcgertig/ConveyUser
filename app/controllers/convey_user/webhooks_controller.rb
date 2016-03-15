4676fefbdca4bc5e1a6dac9eab233dab89f18f00
Jonathan Gertig <jcgertig@gmail.com>
2016-03-15 12:20:07 -0400

    update user ref

--- app/controllers/convey_user/webhooks_controller.rb
+++ app/controllers/convey_user/webhooks_controller.rb
@@ -33,7 +33,7 @@ module ConveyUser
         user = params[:user]
         uid  = user[:_id]
         user.delete(:_id)
-        User.where(uid: uid).first_or_create(user).update(user)
+        ::User.where(uid: uid).first_or_create(user).update(user)
       end
 
       def create_user(params)
