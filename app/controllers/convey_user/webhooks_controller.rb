70f3856794d95bb7bd7ce6668597469ce61b83a1
Jonathan Gertig <jcgertig@gmail.com>
2016-03-15 12:25:42 -0400

    update params passing

--- app/controllers/convey_user/webhooks_controller.rb
+++ app/controllers/convey_user/webhooks_controller.rb
@@ -5,7 +5,7 @@ module ConveyUser
     def catch
       action = request.headers["X-Convey-Action"] if request.headers["X-Convey-Action"]
       resource = request.headers["X-Convey-Resource"] if request.headers["X-Convey-Resource"]
-      send("#{action}_#{resource}", params)
+      send("#{action}_#{resource}", params.to_h)
     end
 
     private
@@ -41,14 +41,14 @@ module ConveyUser
         user = params[:user]
         uid  = user[:_id]
         user.delete(:_id)
-        User.where(uid: uid).first_or_create(user)
+        ::User.where(uid: uid).first_or_create(user)
       end
 
       def destroy_user(params)
         user = params[:user]
         uid  = user[:_id]
         user.delete(:_id)
-        User.where(uid: uid).first.update(active: false)
+        ::User.where(uid: uid).first.update(active: false)
       end
   end
 end
